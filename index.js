1d1ac36da4a05d518ade00859a02b6a5f6a36ea5
Tanarat <spiritsquare@gmail.com>
2016-04-07 23:35:50 +0700

    Change ENV var name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
