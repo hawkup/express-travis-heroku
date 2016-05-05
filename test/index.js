e6b4a96822b5dad03d46884134020ed0c34138d5
Tanarat <spiritsquare@gmail.com>
2016-05-05 21:41:27 +0700

    Test coveralls

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('./index').app;
+var app = require('../index').app;
 
 describe('GET index', function () {
   it('response ok', function (done) {
